d697a067ebf0ae799b18b16659ba298328f167c9
testmachine19 <patysson@mail.ru>
2023-10-26 10:44:55 +0300

    Домашнее задание 2.2.4

--- calculations.js
+++ calculations.js
@@ -19,7 +19,19 @@ function getRestAfterDivision(x, y) {
   return x;
 }
 
+function incrementBalance(x) {
+  x++;
+  return x;
+}
+
+function decrementBalance(y) {
+  y--;
+  return y;
+}
+
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2, 4000));
 console.log(getRestAfterDivision(7000, 3));
+console.log(incrementBalance(3000)); // Увеличить на 1
+console.log(decrementBalance(700)); // Уменьшить на 1

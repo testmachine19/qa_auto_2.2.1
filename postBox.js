4bd167a54ac6dda44879362b341bb6112ee7d2e8
testmachine19 <patysson@mail.ru>
2023-10-30 20:26:56 +0300

    Домашнее задание 2.3.3

--- postBox.js
+++ postBox.js
@@ -1,28 +1,29 @@
-const userBalance = 500;
+let userBalance = 500;
 const packageCost = 100;
 const smsCode = "A001DFX0";
-const postBoxes = [null, null, null, "1432HGF", null];
+const postBox = [null, null, null, "1432HGF", null];
 
-const packageData = {
+let dataBase = {
   "1432HGF": {
+    ID: "1432HGF",
     cellNumber: 4,
-    cost: 100,
+    cost: packageCost,
   },
 };
 
-if (smsCode in packageData) {
-  const packageGetting = packageData[smsCode];
-
-  if (userBalance >= packageGetting.cost) {
-    userBalance -= packageGetting.cost;
-
-    postBoxes[packageGetting.cellNumber] = smsCode;
+if (postBox.includes(smsCode) && dataBase[smsCode]) {
+  const packageInfo = dataBase[smsCode];
+  if (userBalance >= packageInfo.cost) {
+    userBalance -= packageInfo.cost;
+    postBox[postBox.indexOf(smsCode)] = null;
     console.log(
-      `Заберите посылку ${smsCode} из ячейки №${packageInfo.cellNumber}. Ваш счет составляет: ${userBalance}ед.`
+      `Заберите посылку ${packageInfo.ID} из ячейки №${packageInfo.cellNumber}. Ваш счет составляет: ${userBalance}ед.`
     );
   } else {
-    console.log("На вашем счете недостаточно средств для получения посылки.");
+    console.log("Недостаточно средств на балансе для получения посылки.");
   }
 } else {
-  console.log("Посылка с указанным кодом не найдена в базе данных.");
+  console.log(
+    "Код смс не соответствует коду в базе данных или посылка отсутствует в ячейке."
+  );
 }
